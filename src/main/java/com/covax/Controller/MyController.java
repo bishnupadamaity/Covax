23ba060efb4aed2fff61af2d37122f8c823bbe84
Bishnupada Maity <bishnupdm.1@gmail.com>
2022-10-24 12:46:43 +0530

    Set State and District value in form

--- src/main/java/com/covax/Controller/MyController.java
+++ src/main/java/com/covax/Controller/MyController.java
@@ -1,13 +1,20 @@
 package com.covax.Controller;
 
+import com.covax.Services.DataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyController {
-	
+	@Autowired
+	DataService dataService;
 	@RequestMapping("/")
-	public String home() {
+	public String home(Model model) {
+
+		model.addAttribute("allStates",dataService.getAllStates());
+		model.addAttribute("allDistricts",dataService.getAllDistricts());
 		return "index";
 	}
 
--- src/main/java/com/covax/Services/DataService.java
+++ src/main/java/com/covax/Services/DataService.java
@@ -1,61 +1,77 @@
 package com.covax.Services;
 
+import com.covax.Model.District;
 import com.covax.Model.State;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.time.Duration;
+
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
 @Service
 public class DataService {
-    URI uri = URI.create("https://cdn-api.co-vin.in/api/v2/admin/location/states");
+//    URI s_uri = URI.create("https://cdn-api.co-vin.in/api/v2/admin/location/states");
 
     private List<State> allStates = new ArrayList<>();
+    private List<District> allDistricts = new ArrayList<>();
+
+
+    public List<District> getAllDistricts() {
+        return allDistricts;
+    }
+
     public List<State> getAllStates(){
         return allStates;
     }
 
+
+
     @PostConstruct
     public void fetchStateData() throws IOException, InterruptedException {
-
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest
                 .newBuilder()
                 .GET()
-                .uri(uri)
+                .uri(URI.create("https://cdn-api.co-vin.in/api/v2/admin/location/states"))
                 .header("Accept","application/json")
                 .build();
-
         HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
 
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        System.out.println(response.body());
-
         JSONObject obj = new JSONObject(response.body());
-
-        List<String> list = new ArrayList<String>();
         JSONArray array = obj.getJSONArray("states");
         for(int i = 0 ; i < array.length() ; i++){
-            list.add(array.getJSONObject(i).getString("state_name"));
+            State state = new State();
+            state.setId(String.valueOf(array.getJSONObject(i).getInt("state_id")));
+            state.setName(array.getJSONObject(i).getString("state_name"));
+            allStates.add(state);
         }
+    }
+    @PostConstruct
+    public void fetchDistrictData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest
+                .newBuilder()
+                .GET()
+                .uri(URI.create("https://cdn-api.co-vin.in/api/v2/admin/location/districts/26"))
+                .header("Accept","application/json")
+                .build();
+        HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
 
-//        System.out.println(Arrays.toString(list.toArray()));
-//        list.forEach(System.out::println);
-
+        JSONObject obj = new JSONObject(response.body());
+        JSONArray array = obj.getJSONArray("districts");
+        for(int i = 0 ; i < array.length() ; i++){
+            District district = new District();
+            district.setId(String.valueOf(array.getJSONObject(i).getInt("district_id")));
+            district.setName(array.getJSONObject(i).getString("district_name"));
+            allDistricts.add(district);
+        }
     }
 }
